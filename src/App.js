2b04513b42ea1e202adf0dc5ee544fa01b11a997
Arthur Pimenta <92745578+Orelhals@users.noreply.github.com>
2022-07-25 15:45:14 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import { GlobalStyles } from "./styles/Global";
+
+export default function App() {
+  return (
+    <div className="App">
+      <GlobalStyles />
+      <h1>Hello CodeSandbox</h1>
+    </div>
+  );
+}
---
+++ src/styles/Global.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    body {
+      background-color: #efefef;
+      padding: 0;
+      margin: 0;
+      box-sizing: border-box;
+      
+    }
+ `;
