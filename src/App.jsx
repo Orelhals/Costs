798d06a295746f6fea0295c998e6547dfea506dd
Arthur Pimenta <92745578+Orelhals@users.noreply.github.com>
2022-07-26 18:18:16 -0300

    Work in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./components/pages/Home";
 import { Contact } from "./components/pages/Contact";
 import { Company } from "./components/pages/Company";
 import { NewProject } from "./components/pages/NewProject";
+import { Navbar } from "./components/container/Navbar";
+import { Footer } from "./components/container/Footer";
+
+import MainStyle from "./styles/MainStyle";
+import { GlobalStyles } from "./styles/Global";
 
 export const App = () => {
   // return <div>teste</div>;
   return (
     <BrowserRouter>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/company" element={<Company />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/newproject" element={<NewProject />} />
-      </Routes>
+      <GlobalStyles />
+      <Navbar />
+      <MainStyle>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </MainStyle>
+      <Footer />
     </BrowserRouter>
   );
 };
---
+++ src/styles/MainStyle.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const MainStyle = styled.div`
+  background-color: #efe9ef;
+  width: 1200px;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  flex-wrap: wrap;
+`;
+
+export default MainStyle;
